19554132dae1313ccd06f318bab61d880d4b6d92
Bahaa Tantaoui <tantaouibahaabusiness@gmail.com>
2023-12-17 16:29:23 +0100

    example uses

--- src/components/Demonstration.jsx
+++ src/components/Demonstration.jsx
@@ -3,6 +3,7 @@ import Bottle from "../assets/examples/2.jpeg";
 import Headphones from "../assets/examples/3.png";
 import Coffee from "../assets/examples/4.jpg";
 import Laptop from "../assets/examples/5.jpg";
+import "../App.css";
 
 export function Demonstration() {
   const example = [
@@ -10,10 +11,8 @@ export function Demonstration() {
       Product: "Smart Fitness Tracker",
       Description:
         "Elevate your fitness journey with the Smart Fitness Tracker – a cutting-edge wearable designed to seamlessly integrate with your active lifestyle. This sleek device not only monitors your daily activities and tracks workouts but also provides real-time health insights to empower your wellness goals. Stay motivated with personalized fitness recommendations and receive notifications right on the vibrant OLED screen. With its intuitive design and advanced features, the Smart Fitness Tracker is more than a device; it's a fitness revolution.",
-
       Image: Fitness,
     },
-
     {
       Product: "Eco-Friendly Reusable Water Bottle",
       Description:
@@ -38,16 +37,18 @@ export function Demonstration() {
       Product: "Multi-Function Laptop Stand",
       Description:
         "Transform your workspace with our Multi-Function Laptop Stand – a sleek solution that combines functionality with elegance. Designed for both comfort and productivity, this adjustable stand offers a range of height and angle options to create your ideal working environment. Say goodbye to discomfort and overheating as you elevate your laptop to a new level of efficiency. Whether you're a remote worker or a student, this stand is the epitome of style and support, making every task a seamless and enjoyable experience.",
-
       Image: Laptop,
     },
   ];
+
   return (
     <>
       <div className="m-4 grid grid-cols-3 gap-4">
         {example.map((product, index) => (
           <div key={index} className="border p-4">
-            <img src={product.Image} alt="product image" />
+            <div className="image-container-examples2">
+              <img src={product.Image} alt="product image" />
+            </div>
             <h2 className="text-lg font-semibold mb-2">{product.Product}</h2>
             <p>{product.Description}</p>
           </div>
@@ -56,7 +57,9 @@ export function Demonstration() {
       <div className="m-4 grid grid-cols-2 gap-4">
         {Example2.map((product, index) => (
           <div key={index} className="border p-4">
-            <img src={product.Image} alt="product image" />
+            <div className="image-container-examples1">
+              <img src={product.Image} alt="product image" />
+            </div>
             <h2 className="text-lg font-semibold mb-2">{product.Product}</h2>
             <p>{product.Description}</p>
           </div>
